83bb2a4fffe8e3d633f31dc1370b36df2f0a5543
liuc12580 <958616386@qq.com>
2020-08-19 10:52:58 +0800

    打个招呼

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.jk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+
+    @RequestMapping("/hi")
+    public String hi(){
+        return "hi wuminqiang";
+    }
+
 }
